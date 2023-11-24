c6dc4b3190b29a2930a85ca61bab3f3c88f40984
William-Almeida <williamkillerca@hotmail.com>
2023-11-24 01:46:53 -0300

    Adicionando flatpickr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 import './bootstrap';
 
+import flatpickr from "flatpickr";
+import { Portuguese } from "flatpickr/dist/l10n/pt.js"
+import 'flatpickr/dist/flatpickr.min.css'
+
 // import Alpine from 'alpinejs';
-
 // window.Alpine = Alpine;
-
-// Alpine.start();
 
 import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
  
@@ -13,3 +14,5 @@ import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css'
 
 // If you use Bootstrap 5 
 // import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
+
+// Alpine.start();
