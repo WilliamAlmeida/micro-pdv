4bb7f90d9af52c26c5beca02d74c98d2f099a36d
William-Almeida <williamkillerca@hotmail.com>
2023-11-24 23:12:32 -0300

    Importando tailwindcss/colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ import forms from '@tailwindcss/forms';
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import typography from '@tailwindcss/typography';
 
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 export default {
     presets: [
-        require('./vendor/wireui/wireui/tailwind.config.js')
+        require('./vendor/wireui/wireui/tailwind.config.js'),
+        require("./vendor/power-components/livewire-powergrid/tailwind.config.js"), 
     ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -16,15 +19,16 @@ export default {
         './vendor/wireui/wireui/ts/**/*.ts',
         './vendor/wireui/wireui/src/View/**/*.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "pg-primary": colors.gray, 
+            },
         },
     },
-
     plugins: [
         forms({
             strategy: 'class',
@@ -32,6 +36,5 @@ export default {
         aspectRatio,
         typography
     ],
-
     darkMode: 'class',
 };
