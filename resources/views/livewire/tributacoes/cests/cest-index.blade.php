cb637a4574f0e9667c67e3d3c682eb5b0c143dcd
William-Almeida <williamkillerca@hotmail.com>
2023-11-30 01:26:42 -0300

    Corrigindo espaçamento na importação dos components

--- resources/views/livewire/tributacoes/cests/cest-index.blade.php
+++ resources/views/livewire/tributacoes/cests/cest-index.blade.php
@@ -13,7 +13,7 @@
 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <livewire:tributacoes.cests.cest-table/>
+                    <livewire:tributacoes.cests.cest-table />
                 </div>
             </div>
         </div>
--- resources/views/livewire/tributacoes/ncms/ncm-index.blade.php
+++ resources/views/livewire/tributacoes/ncms/ncm-index.blade.php
@@ -13,7 +13,7 @@
 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <livewire:tributacoes.ncms.ncm-table/>
+                    <livewire:tributacoes.ncms.ncm-table />
                 </div>
             </div>
         </div>
