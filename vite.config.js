f88d3394ef7b9906bfe1df006bb246805ebf13a6
William-Almeida <williamkillerca@hotmail.com>
2024-01-09 11:14:32 -0300

    Atualizando vite e laravel-vite-plugin do projeto

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     plugins: [
+        viteStaticCopy({
+            targets: [
+                {src: 'resources/plugins/cleave/cleave.min.js', dest: 'assets/plugins'}
+            ]
+        }),
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/plugins/cleave.min.js',
             ],
             refresh: true,
         }),
