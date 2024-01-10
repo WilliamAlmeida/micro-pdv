06b59ac605946f5c3636d25b83c9a3c0b873ed6c
William-Almeida <williamkillerca@hotmail.com>
2024-01-10 12:30:04 -0300

    Adicionando informativo de Convênio Vendido

--- resources/views/livewire/pdv/vendas/includes/infoSaleGrid.blade.php
+++ resources/views/livewire/pdv/vendas/includes/infoSaleGrid.blade.php
@@ -3,19 +3,22 @@
 
     <div class="flex flex-wrap sm:flex-row justify-around text-center pb-4 px-2">
         <div class="self-center">
-            <span class="text-base sm:text-2xl"><strong>Dinheiro</strong><br/>R$ @money($caixa->pagamentos['dinheiro'] ?? 0)</span>
+            <span class="text-base sm:text-xl"><strong>Dinheiro</strong><br/>R$ @money($caixa->pagamentos['dinheiro'] ?? 0)</span>
         </div>
         <div class="self-center">
-            <span class="text-base sm:text-2xl"><strong>Ticket</strong><br/>R$ @money($caixa->pagamentos['ticket'] ?? 0)</span>
+            <span class="text-base sm:text-xl"><strong>Ticket</strong><br/>R$ @money($caixa->pagamentos['ticket'] ?? 0)</span>
         </div>
         <div class="self-center">
-            <span class="text-base sm:text-2xl"><strong>Débito</strong><br/>R$ @money($caixa->pagamentos['cartao_debito'] ?? 0)</span>
+            <span class="text-base sm:text-xl"><strong>Débito</strong><br/>R$ @money($caixa->pagamentos['cartao_debito'] ?? 0)</span>
         </div>
         <div class="self-center">
-            <span class="text-base sm:text-2xl"><strong>Crédito</strong><br/>R$ @money($caixa->pagamentos['cartao_credito'] ?? 0)</span>
+            <span class="text-base sm:text-xl"><strong>Crédito</strong><br/>R$ @money($caixa->pagamentos['cartao_credito'] ?? 0)</span>
         </div>
         <div class="self-center">
-            <span class="text-base sm:text-2xl"><strong>Total Geral</strong><br/>R$ @money($caixa->pagamentos->sum() ?? 0)</span>
+            <span class="text-base sm:text-xl"><strong>Convênio Vend.</strong><br/>R$ @money($caixa->pagamentos['convenio'] ?? 0)</span>
+        </div>
+        <div class="self-center">
+            <span class="text-base sm:text-xl"><strong>Total Geral</strong><br/>R$ @money($caixa->pagamentos->sum() ?? 0)</span>
         </div>
     </div>
 </div>
