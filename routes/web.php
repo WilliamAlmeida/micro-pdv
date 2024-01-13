e17046711c57653c51043b163fbc07d89b3b9a0f
William-Almeida <williamkillerca@hotmail.com>
2024-01-12 23:26:48 -0300

    Removendo middleware verified das demais rotas

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,10 @@ Route::prefix('painel')->group(function () {
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 
-    Route::middleware(['auth', 'verified'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::get('/dashboard', function () {
             return view('dashboard');
-        })->name('dashboard');
+        })->name('dashboard')->middleware('verified');
     
         Route::get('/usuarios', UsuarioIndex::class)->name('usuarios.index');
 
