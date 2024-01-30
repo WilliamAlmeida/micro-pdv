3f0b753381fbfea4e0447618bf50498d34f27ea3
William-Almeida <williamkillerca@hotmail.com>
2024-01-30 10:05:41 -0300

    Melhorando visual do e-mail de convite

--- resources/views/emails/tenant-invite-user.blade.php
+++ resources/views/emails/tenant-invite-user.blade.php
@@ -2,7 +2,7 @@
 <p>Espero que este e-mail o encontre bem.</p>
 <p>Gostaríamos de estender a você um convite para se juntar à equipe da {{ $tenant_name }}! Após revisarmos seu perfil e suas habilidades, acreditamos que você seria uma excelente adição ao nosso time.</p>
 <p>Na {{ $tenant_name }}, estamos comprometidos em oferecer um ambiente de trabalho dinâmico e desafiador, onde cada membro da equipe tem a oportunidade de crescer profissionalmente e contribuir para o sucesso da empresa.</p>
-<p>Para aceitar nosso convite e começar sua jornada conosco, basta clicar em <a href="{{ $link_accept }}" target="_blank">Aceitar Convite</a> ou copiando a url <strong>{{ $link_accept }}</strong> e colando no navegador.</p>
+<p>Para aceitar nosso convite e começar sua jornada conosco, basta clicar em <a href="{{ $link_accept }}" target="_blank">Aceitar Convite</a> ou copiando a url abaixo e colando no navegador:<br/><br/>{{ $link_accept }}<br/><br/></p>
 <p>Após clicar no link, você será redirecionado para o nosso portal, onde poderá acessar seu painel e iniciar suas atividades conosco.</p>
 <p>Estamos ansiosos para receber você em nossa equipe e trabalhar juntos para alcançar grandes conquistas.</p>
 <p>Se houver alguma dúvida ou precisar de mais informações, não hesite em entrar em contato conosco.</p>
@@ -11,6 +11,7 @@
 <p>Atenciosamente,</p>
 <p>
     {{ $tenant->users->first()->name }}<br>
+    <br>
     CEO<br>
     {{ $tenant_name }}
 </p>
