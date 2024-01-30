4e58c09f1ba8b351d82b47d134146043f40dcdad
William-Almeida <williamkillerca@hotmail.com>
2024-01-30 11:40:11 -0300

    Removendo variavel sem uso

--- app/Livewire/Tenant/Categorias/CategoriaIndex.php
+++ app/Livewire/Tenant/Categorias/CategoriaIndex.php
@@ -7,14 +7,6 @@ use Livewire\Component;
 
 class CategoriaIndex extends Component
 {
-    #[Locked]
-    public $tenant;
-
-    public function mount()
-    {
-        $this->tenant = tenant();
-    }
-
     public function render()
     {
         return view('livewire.tenant.categorias.categoria-index');
